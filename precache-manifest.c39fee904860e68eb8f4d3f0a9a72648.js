17886d812ac973774311c45eb0c47cccd10d9347
Flash KIM <62537294+Flash-KIM142@users.noreply.github.com>
2020-08-18 16:45:41 +0900

    Updates

---
+++ precache-manifest.c39fee904860e68eb8f4d3f0a9a72648.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32966d4a03329a8122f1429ed65bbb8e",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "40329a6877e4b27987be",
+    "url": "/react_todo/static/js/2.4d31d534.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_todo/static/js/2.4d31d534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874b75b62b7ac32ad8c4",
+    "url": "/react_todo/static/js/main.a22c5139.chunk.js"
+  },
+  {
+    "revision": "f0f7b3c8281a6f41d057",
+    "url": "/react_todo/static/js/runtime-main.f3caa06c.js"
+  }
+]);
